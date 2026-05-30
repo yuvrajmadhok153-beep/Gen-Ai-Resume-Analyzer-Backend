1dd2d4bdfc11085c75b40cd4c75aad89d2e53580
YURAJ MADHOK <yuvrajmadhok153@gmail.com>
2026-05-30 15:47:43 +0530

    update urls of frontend

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://gen-ai-resume-analyzer-frontend.vercel.app",
     credentials: true
 }))
 
